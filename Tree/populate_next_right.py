cd5d16a7e32eae1543a2c04c54b174f8efec14a7
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-06-03 18:31:52 +0900

    Add more problem

--- Tree/populate_next_right.py
+++ Tree/populate_next_right.py
@@ -11,6 +11,19 @@ class Node:
         self.next = next
 
 class Solution:
+    def calculate_depth(self, root):
+        if root is not None:
+            return 1 + self.calculate_depth(root.left)
+        return 0
+
+    def get_nodes_of_level(self, root, current_level, target_level):
+        if current_level == target_level:
+            return [root]
+        else:
+            left_nodes = self.get_nodes_of_level(root.left, current_level+1, target_level)
+            right_nodes = self.get_nodes_of_level(root.right, current_level+1, target_level)
+            return left_nodes + right_nodes
+
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if root is not None:
             print(root.val)
@@ -25,4 +38,7 @@ root.left.right = Node(5)
 root.right.left = Node(6)
 root.right.right = Node(7)
 sol = Solution()
-sol.connect(root)
+# sol.connect(root)
+for node in sol.get_nodes_of_level(root, 1, 3):
+    print(node.val)
+print(sol.get_nodes_of_level(root, 1, 3))
