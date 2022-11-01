e8e83abc06fe7c5b6932d6c46f18358ce3d5ef53
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2022-11-01 09:05:13 +0900

    Time: 496 ms (39.16%), Space: 14.6 MB (19.15%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        outcome = []
+        for _ in range(len(grid) + 1):
+            outcome.append([None] * len(grid[0]))
+
+        for i in range(len(outcome)):
+            for j in range(len(outcome[i])):
+                if i == 0:
+                    outcome[i][j] = j
+                else:
+                    last_pos = outcome[i-1][j]
+                    if last_pos == -1:
+                        outcome[i][j] = -1
+                    else:
+
+                        if (last_pos == 0 and grid[i-1][last_pos] == -1) or (last_pos == len(grid[i-1]) - 1 and grid[i-1][last_pos] == 1):
+                            outcome[i][j] = -1
+                        elif (last_pos > 0 and grid[i-1][last_pos-1] == 1 and grid[i-1][last_pos] == -1) or (last_pos < len(grid[i-1]) - 1 and grid[i-1][last_pos] == 1 and grid[i-1][last_pos+1] == -1):
+                            outcome[i][j] = -1
+                        else:
+                            outcome[i][j] = last_pos + grid[i-1][last_pos]
+
+        return outcome[-1]
