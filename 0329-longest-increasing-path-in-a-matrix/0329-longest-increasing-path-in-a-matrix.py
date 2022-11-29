a8fb330c7a00f095b653ed2340436e3868ce97de
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2022-11-29 16:06:04 +0900

    Time: 3608 ms (5.00%), Space: 14.5 MB (96.90%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,60 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        n, m = len(matrix), len(matrix[0])
+        memory = [[0 for _ in range(m)] for _ in range(n)]
+        for _ in range(20):
+            for i in range(n):
+                for j in range(m):
+                    if i==0 and j==0:
+                        memory[i][j] = max(memory[i][j], 1)
+                    else:
+                        possible = 1
+                        if i==0:
+                            if matrix[i][j] < matrix[i][j-1]:
+                                possible += memory[i][j-1]
+                        elif j==0:
+                            if matrix[i][j] < matrix[i-1][j]:
+                                possible += memory[i-1][j]
+                        else:
+                            max_add = 0
+                            if matrix[i][j] < matrix[i][j-1]:
+                                max_add = max(max_add, memory[i][j-1])
+                            if matrix[i][j] < matrix[i-1][j]:
+                                max_add = max(max_add, memory[i-1][j])
+                            possible += max_add
+                        memory[i][j] = max(memory[i][j], possible)
+
+            # for a in memory:
+            #     print(a)
+
+            for i in range(n-1,-1,-1):
+                for j in range(m-1,-1,-1):
+                    if i==n-1 and j==m-1:
+                        memory[i][j] = max(memory[i][j], 1)
+                    else:
+                        possible = 1
+                        if i==n-1:
+                            if matrix[i][j] < matrix[i][j+1]:
+                                possible += memory[i][j+1]
+                        elif j==m-1:
+                            if matrix[i][j] < matrix[i+1][j]:
+                                possible += memory[i+1][j]
+                        else:
+                            max_add = 0
+                            if matrix[i][j] < matrix[i][j+1]:
+                                max_add = max(max_add, memory[i][j+1])
+                            if matrix[i][j] < matrix[i+1][j]:
+                                max_add = max(max_add, memory[i+1][j])
+                            possible += max_add
+                        memory[i][j] = max(memory[i][j], possible)
+
+            # for a in memory:
+            #     print(a)
+        
+        max_length = 0
+        for i in range(n):
+            for j in range(m):
+                max_length = max(max_length, memory[i][j])
+        return max_length
+        
+        
