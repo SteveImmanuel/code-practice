fa6f6b23d3e15125c747325bcc36b89aaeac391f
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2023-04-06 11:27:26 +0900

    Time: 150 ms (16.49%), Space: 15.1 MB (14.25%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -0,0 +1,45 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        zeros_left = set()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    zeros_left.add((i, j))
+                    
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        queue = deque([])
+        
+        if len(zeros_left) > 0:
+            item = zeros_left.pop()
+            zeros_left.add(item)
+            queue = deque([item])
+        
+        valid = True
+        total_closed = 0
+        while len(queue) > 0:
+            i, j = queue.popleft()
+            # print(i < 0, i >= m, j < 0, j >= n, (i, j) not in zeros_left, grid[i][j] != 0)
+            if i < 0 or i >= m or j < 0 or j >= n or (i, j) not in zeros_left or grid[i][j] != 0:
+                if len(queue) == 0:
+                    if valid:
+                        total_closed += 1
+                    
+                    if len(zeros_left) > 0:
+                        item = zeros_left.pop()
+                        zeros_left.add(item)
+                        queue.append(item)
+                        valid = True
+                
+                
+                continue
+                
+            zeros_left.remove((i, j))
+            if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                valid = False
+                
+            for delta in dirs:
+                queue.append((i + delta[0], j + delta[1]))
+            # print(queue)
+            
+        return total_closed
