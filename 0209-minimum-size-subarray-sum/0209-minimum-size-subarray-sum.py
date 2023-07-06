001ceee1ddea285bcabe4d7bfa76eb3bb39f1ef9
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2023-07-06 10:12:50 +0900

    Time: 272 ms (37.77%), Space: 29.1 MB (41.05%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -11,30 +11,64 @@ class Solution:
 #                 h = l
 #         return l
     
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        prefix_sum = []
-        # loc_dict = defaultdict(list) # needed if the nums[i] can be negative to handle multiple same values
-        # since the nums[i] >= 1, the prefix sum is guaranteed unique
-        total = 0
-        for i, num in enumerate(nums):
-            total += num
-            prefix_sum.append(total)
+    # O(n log n) solution
+#     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+#         prefix_sum = []
+#         # loc_dict = defaultdict(list) # needed if the nums[i] can be negative to handle multiple same values
+#         # since the nums[i] >= 1, the prefix sum is guaranteed unique
+#         total = 0
+#         for i, num in enumerate(nums):
+#             total += num
+#             prefix_sum.append(total)
 
-        min_length = None
-        for i in range(len(nums)):
-            if prefix_sum[i] < target:
-                continue
+#         min_length = None
+#         for i in range(len(nums)):
+#             if prefix_sum[i] < target:
+#                 continue
             
-            budget = prefix_sum[i] - target
-            cur_min = i + 1
+#             budget = prefix_sum[i] - target
+#             cur_min = i + 1
 
-            if budget > 0:
-                idx = bisect_right(prefix_sum, budget, 0, i + 1)
-                if idx >= 0:
-                    cur_min -= idx
-            if min_length is None or cur_min < min_length:
-                min_length = cur_min
+#             if budget > 0:
+#                 idx = bisect_right(prefix_sum, budget, 0, i + 1)
+#                 if idx >= 0:
+#                     cur_min -= idx
+#             if min_length is None or cur_min < min_length:
+#                 min_length = cur_min
         
-        if min_length is None:
-            return 0
-        return min_length
+#         if min_length is None:
+#             return 0
+#         return min_length
+    
+    # O(n) solution
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l, r = 0, 0
+        cur_sum = 0
+        min_length = None
+        
+        while r < len(nums):
+            cur_sum += nums[r]
+            
+            while cur_sum - nums[l] >= target and l + 1 <= r:
+                cur_sum -= nums[l]
+                l += 1
+            
+            if cur_sum >= target:
+                cur_min = r - l + 1
+                if min_length is None or cur_min < min_length:
+                    min_length = cur_min    
+            
+            r += 1
+                
+        return min_length if min_length else 0              
+                
+                
+                
+                
+                
+                
+                
+        
+        
+        
+        
