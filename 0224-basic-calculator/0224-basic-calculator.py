38f8fe2fdf5f593822956644a73126b3bcae9108
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2023-12-19 16:09:53 +0900

    Time: 88 ms (48.29%), Space: 18.1 MB (37.89%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -7,53 +7,9 @@ class Solution:
         
     def calculate(self, s: str) -> int:
         res = self._calc(s, 0)
-        # print(res)
         return res[1]
 
-    def _calc(self, s, idx):
-        # print('evaluating', s[idx:])
-        f_num = 0
-        s_num = None
-        op = None
-        while idx < len(s):
-            # print('idx:', idx, 'char:', s[idx])
-            # print('before', f_num, s_num, op)
-
-            if s[idx] in ['+', '-']:
-                if s_num is not None:
-                    if op is not None:
-                        f_num = self.atomic(f_num, op, s_num)
-                        op = None
-                    else:
-                        f_num = s_num
-                    s_num = None
-                op = s[idx]
-                idx += 1
-            elif s[idx].isnumeric():
-                if s_num is None:
-                    s_num = int(s[idx])
-                else:
-                    s_num = s_num * 10 + int(s[idx])
-                idx += 1
-            elif s[idx] == '(':
-                assert s_num is None
-                idx, s_num = self._calc(s, idx + 1)
-                if op is not None:
-                    f_num = self.atomic(f_num, op, s_num)
-                    op = None
-                else:
-                    f_num = s_num
-                s_num = None
-            elif s[idx] == ')':
-                idx += 1
-                break
-            else:
-                idx += 1
-            
-            # print('after', f_num, s_num, op)
-            # print()
-
-        # print('before', f_num, s_num, op)
+    def _eval(self, f_num, op, s_num):
         if s_num is not None:
             if op is not None:
                 f_num = self.atomic(f_num, op, s_num)
@@ -61,6 +17,31 @@ class Solution:
             else:
                 f_num = s_num
             s_num = None
-        # print('after', f_num, s_num, op)
+        return f_num, op, s_num
+
+    def _calc(self, s, idx):
+        f_num = 0
+        s_num = None
+        op = None
+        while idx < len(s):
+            if s[idx] in ['+', '-']:
+                f_num, op, s_num = self._eval(f_num, op, s_num)
+                op = s[idx]
+            elif s[idx].isnumeric():
+                if s_num is None:
+                    s_num = int(s[idx])
+                else:
+                    s_num = s_num * 10 + int(s[idx])
+            elif s[idx] == '(':
+                idx, s_num = self._calc(s, idx + 1)
+                f_num, op, s_num = self._eval(f_num, op, s_num)
+                idx -= 1
+            elif s[idx] == ')':
+                idx += 1
+                break
+            
+            idx += 1
+        
+        f_num, op, s_num = self._eval(f_num, op, s_num)
         
         return idx, f_num
