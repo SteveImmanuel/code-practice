2f73638c630e5e90cc5f1f206b765834944daa70
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2024-05-10 10:41:20 +0800

    Time: 2153 ms (18.30%), Space: 68.7 MB (52.54%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
@@ -0,0 +1,24 @@
+class Solution:
+    # def get_kth_number(self, heap, k):
+    #     print(heap, k)
+    #     buffer = []
+    #     while k > 0:
+    #         res = heapq.heappop(heap)
+    #         buffer.append(res)
+    #         k -= 1
+    #     while len(buffer) > 0:
+    #         heapq.heappush(heap, buffer.pop())
+    #     return res
+            
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        heap = []
+        for d_idx in range(len(arr) - 1, 0, -1):
+            found = False
+            for n_idx in range(d_idx):
+                heapq.heappush(heap, (arr[n_idx] / arr[d_idx], arr[n_idx], arr[d_idx]))
+        
+        
+        while k > 0:
+            res = heapq.heappop(heap)
+            k -= 1
+        return [res[1], res[2]]
