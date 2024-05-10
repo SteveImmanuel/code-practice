22f86aeeb640f5888255178822e687aac8ee3b81
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2024-05-10 10:55:34 +0800

    Time: 831 ms (51.86%), Space: 61.4 MB (52.88%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
@@ -10,15 +10,29 @@ class Solution:
     #         heapq.heappush(heap, buffer.pop())
     #     return res
             
+#     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+#         heap = []
+#         for d_idx in range(len(arr) - 1, 0, -1):
+#             found = False
+#             for n_idx in range(d_idx):
+#                 heapq.heappush(heap, (arr[n_idx] / arr[d_idx], arr[n_idx], arr[d_idx]))
+        
+        
+#         while k > 0:
+#             res = heapq.heappop(heap)
+#             k -= 1
+#         return [res[1], res[2]]
+    
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        heap = []
-        for d_idx in range(len(arr) - 1, 0, -1):
-            found = False
-            for n_idx in range(d_idx):
-                heapq.heappush(heap, (arr[n_idx] / arr[d_idx], arr[n_idx], arr[d_idx]))
+        heap = [(arr[0] / arr[i], arr[0], arr[i], 0, i) for i in range(len(arr) - 1, 0, -1)]
+        heapq.heapify(heap)
         
+        res = []
+        while len(res) < k:
+            _, n, d, n_idx, d_idx = heapq.heappop(heap)
+            res.append([n, d])
+            if n_idx + 1 < d_idx:
+                heapq.heappush(heap, (arr[n_idx+1]/ arr[d_idx], arr[n_idx+1], arr[d_idx], n_idx+1, d_idx))
         
-        while k > 0:
-            res = heapq.heappop(heap)
-            k -= 1
-        return [res[1], res[2]]
+        return res[-1]
+            
