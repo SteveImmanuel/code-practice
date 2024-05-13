25172a7887f4a75615453723dcd64451dafa7c07
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2024-05-13 09:39:37 +0800

    Time: 33 ms (95.69%), Space: 16.6 MB (59.05%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -6,19 +6,13 @@ class Solution:
                 for j in range(n):
                     grid[i][j] = abs(grid[i][j] - 1)
 
-        for j in range(n):
+        total = (2 ** (n - 1)) * m
+        for j in range(1, n):
             count_1 = m
             for i in range(m):
                 if grid[i][j] == 0:
                     count_1 -= 1
-            count_0 = m - count_1
-            if count_1 < count_0:
-                for i in range(m):
-                    grid[i][j] = abs(grid[i][j] - 1)
+            mult = max(count_1, m - count_1)
+            total += (2 ** (n - 1 - j)) * mult
         
-        total = 0
-        for i in range(m):
-            num = ''.join(map(str, grid[i]))
-            num = int('0b' + num, 2)
-            total += num
         return total
