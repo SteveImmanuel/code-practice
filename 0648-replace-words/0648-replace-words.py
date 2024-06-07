224603bb1ddbb57dbaa4e7353faf1d49598373e1
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2024-06-07 12:03:29 +0900

    Time: 104 ms (68.50%), Space: 36.6 MB (18.82%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,38 +1,39 @@
 class TrieNode:
-    def __init__(self, val, children={}, index=[]):
+    def __init__(self, val, children={}, is_word=False):
         self.val = val
         self.children = children
-        self.index = index
+        self.is_word = is_word
         
-    def add_word(self, word, index):
+    def add_word(self, word):
         cur_root = self
         for c in word:
             if c not in cur_root.children:
                 new_child = TrieNode(c, {}, [])
                 cur_root.children[c] = new_child
             cur_root = cur_root.children[c]
-            cur_root.index.append(index)
+        cur_root.is_word = True
     
-    def get_indices(self, word):
+    def get_shortest(self, word):
         cur_root = self
+        result = []
         for c in word:
             if c in cur_root.children:
+                result.append(c)
                 cur_root = cur_root.children[c]
+                if cur_root.is_word:
+                    return ''.join(result)
             else:
-                return []
-        return cur_root.index
-        
+                return word
+        return word
 
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         prefix_tree = TrieNode('', {}, [])
-        result = sentence.split(' ')
-        for i, word in enumerate(result):
-            prefix_tree.add_word(word, i)
-        
         for word in dictionary:
-            indices = prefix_tree.get_indices(word)
-            for item in indices:
-                if len(word) < len(result[item]):
-                    result[item] = word
+            prefix_tree.add_word(word)
+        
+        result = sentence.split(' ')
+        for i in range(len(result)):
+            result[i] = prefix_tree.get_shortest(result[i])
+        
         return ' '.join(result)
