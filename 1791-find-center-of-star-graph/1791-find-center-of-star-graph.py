d9682754bf8172543d6b31dcee0896a2f2329165
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2024-06-27 09:48:58 +0900

    Time: 645 ms (83.80%), Space: 52.2 MB (75.98%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,9 +1,10 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
-        for key in graph:
-            if len(graph[key]) == len(graph) - 1:
-                return key
+        # graph = defaultdict(list)
+        # for a, b in edges:
+        #     graph[a].append(b)
+        #     graph[b].append(a)
+        # for key in graph:
+        #     if len(graph[key]) == len(graph) - 1:
+        #         return key
+        return edges[0][0] if edges[0][0] in edges[1] else edges[0][1]
