f636312eea2e2dbf9328c568d32ec24cd4d7157b
Steve Immanuel <38256664+SteveImmanuel@users.noreply.github.com>
2024-10-18 10:00:32 +0900

    Time: 1283 ms (22.20%), Space: 16.6 MB (44.07%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,18 +1,22 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        max_possible = ['0'] * 17
-        for i in range(16, -1, -1):
-            shift = 16 - i
-            # print(shift)
-            for num in nums:
-                # print(num, shift, num >> shift)
-                if (num >> shift) & 1 == 1:
-                    max_possible[i] = '1'
-                    break
-        # print(max_possible)
-        max_possible = '0b' + ''.join(max_possible)
-        max_possible = int(max_possible, 2)
-        # print(max_possible)
+        # max_possible = ['0'] * 17
+        # for i in range(16, -1, -1):
+        #     shift = 16 - i
+        #     # print(shift)
+        #     for num in nums:
+        #         # print(num, shift, num >> shift)
+        #         if (num >> shift) & 1 == 1:
+        #             max_possible[i] = '1'
+        #             break
+        # # print(max_possible)
+        # max_possible = '0b' + ''.join(max_possible)
+        # max_possible = int(max_possible, 2)
+        # # print(max_possible)
+        
+        max_possible = 0
+        for num in nums:
+            max_possible = max_possible | num
         
         total = 0
         for i in range(1, 2 ** len(nums)):
